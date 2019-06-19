d3772db8731ba7ed1f53727d4609f882a33b852f
Mos <mosyaginivan@gmail.com>
2019-06-19 18:56:15 +0300

    Diffraction fixed

--- src/modeling/Modulating.java
+++ src/modeling/Modulating.java
@@ -2,42 +2,32 @@ package modeling;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-
 public class Modulating {
     public static BufferedImage getCircleImage(int len, double radius) {
         Dimension winSize = new Dimension(500,500);
 
-        Diffraction frame = Diffraction.getCircleDiffraction(winSize,1000, len,200,50,0, radius);
 
-        return getImage(winSize, frame);
     }
 
     public static BufferedImage getSlitImage(int len, double radius) {
         Dimension winSize = new Dimension(500,500);
 
-        Diffraction frame = Diffraction.getSlitDiffraction(winSize,1000, len,200,50,0, radius);
 
-        return getImage(winSize, frame);
     }
 
     public static BufferedImage getDoubleSlitImage(int len, double radius) {
         Dimension winSize = new Dimension(500,500);
 
-        Diffraction frame = Diffraction.getDoubleSlitDiffraction(winSize,1000, len,200,50,0, radius);
 
-        return getImage(winSize, frame);
     }
 
     public static BufferedImage getSquareImage(int len, double radius) {
         Dimension winSize = new Dimension(500,500);
 
-        Diffraction frame = Diffraction.getSquareDiffraction(winSize,1000, len,200,50,0, radius);
 
-        return getImage(winSize, frame);
     }
 
     private static BufferedImage getImage(Dimension imageSize, Diffraction frame) {
-        frame.computeFunction();
 
         int [] pixels = new int[imageSize.width * imageSize.height];
 
